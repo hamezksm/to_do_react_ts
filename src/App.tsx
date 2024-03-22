8a509738d9689696e6b40cf8349594e51c59ed52
hamezksm <jamcav097@gmail.com>
2024-03-22 09:15:20 +0300

    App UI and its styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
+import InputField from "./components/InputField";
+import { Todo } from "./model";
+import TodoList from "./components/TodoList";
 
-function App() {
-  return <div className="App">Hello World!</div>;
-}
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
+
+    console.log(todo);
+  };
+
+  return (
+    <div className="App">
+      <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
+    </div>
+  );
+};
 
 export default App;
